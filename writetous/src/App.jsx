ab92dd6bd646d20a3f6289e02271613a7bbd3a93
Florencia Calvo <florenciacalvor@gmail.com>
2023-09-09 22:39:47 -0300

    primer version

---
+++ writetous/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import './index.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+      </div>
+      <h1>Write To Us</h1>
+      <div className="card">
+        <p>
+          Bienvenido
+        </p>
+      </div>
+    </>
+  )
+}
+
+export default App
